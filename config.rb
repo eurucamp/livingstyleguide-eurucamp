8ca3e1d015d1f79c72a256d6831e0e5e192449a9
Nico Hagenburger <nico@hagenburger.net>
2014-03-16 20:50:34 +0100

    no layout for style guide

--- config.rb
+++ config.rb
@@ -14,7 +14,7 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+page "index.html", layout: false
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
