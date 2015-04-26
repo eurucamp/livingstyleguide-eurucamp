89390fe5bc2a9e7ca211bec9820fd2dabff755d5
Nico Hagenburger <nico@hagenburger.net>
2015-04-26 09:41:11 +0200

    moved templates for build index into separate dir

--- lib/build_changelog_helper.rb
+++ lib/build_changelog_helper.rb
@@ -1,44 +1,15 @@
 class BuildChangelogHelper
 
-  ROBOTS_TXT = <<-EOT
-User-agent: *
-Allow: /latest
-Allow: /index.html
-Disallow: /
-EOT
-
-  TEMPLATE = <<-EOT
-<!DOCTYPE html>
-<html lang="en">
-  <head><meta charset="utf-8"><title>eurucamp living style guide</title></head>
-  <body>
-    <h2><a href="https://github.com/eurucamp/livingstyleguide-eurucamp/">eurucamp/livingstyleguide-eurucamp</a></h2>
-    <ul>
-      <% for year in builds.map { |b| b["branch"] }.uniq.sort.reverse %>
-        <li><a href="<%= year %>/">Latest version for <%= year %></a></li>
-      <% end %>
-    </ul>
-    <dl>
-      <% for build in builds.reverse %>
-        <dt><a href="<%= build["commit"] %>/"><strong><%= build["branch"] %>:</strong> <%= build["commit"] %></a></dt>
-        <dd><strong><%= build["message"] %></strong><br><%= build["author"] %> (<%= build["time"] %>)</dd>
-      <% end %>
-    </dl>
-  </body>
-</html>
-EOT
-
   def head
     %x{git rev-parse --short HEAD}.strip
   end
 
-  def latest_commit
-    %x{git show --pretty=full --name-status HEAD}.strip
+  def build_index_for(builds)
+    years = builds.map { |b| b["branch"] }.uniq
+    Dir.glob("index/*").each do |file|
+      output = Tilt.new(file).render(nil, builds: builds, years: years)
+      File.write(file.sub(/^index(.+)\.[a-z]+$/, 'build/\\1'), output)
+    end
   end
 
-  def build_index_for(builds)
-    output = ERB.new(TEMPLATE).result(binding)
-    File.write('build/index.html', output)
-    File.write('build/robots.txt', ROBOTS_TXT)
-  end
 end
